1b7bee542093eb2b41bdb26b41c40001e3637bf4
David Viramontes <dviramontes@gmail.com>
2017-09-04 16:03:49 -0600

    feat: nav css

--- src/cljs/tmhas/components/navigation.cljs
+++ src/cljs/tmhas/components/navigation.cljs
@@ -6,11 +6,11 @@
       [re-com/h-box
        :gap "1em"
        :children [[:header
-                   {:class "fl w-100 pa3 ph5-ns bg-white ba"}
+                   {:class "fl w-100 pa3 ph5-ns bg-white ba nav"}
                    [:h1 "NAV"]
                    [re-com/h-box
                     :children [[:ul
-                                [:li [:span [link "home" "/"]]]
-                                [:li [:span [link "about" "about"]]]
-                                [:li [:span [link "people" "people"]]]
-                                [:li [:span [link "events" "events"]]]]]]]]])
+                                [:li.ph2 [:span [link "home" "/"]]]
+                                [:li.ph2 [:span [link "about" "about"]]]
+                                [:li.ph2 [:span [link "people" "people"]]]
+                                [:li.ph2 [:span [link "events" "events"]]]]]]]]])
