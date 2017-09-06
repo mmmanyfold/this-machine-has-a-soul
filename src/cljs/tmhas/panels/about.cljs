f4fd1b2cbab7d6da0b449dda2d4f73f0bf57315f
David Viramontes <dviramontes@gmail.com>
2017-09-06 00:21:41 -0600

    fix: about panel sections, iterate through all sections

--- src/cljs/tmhas/panels/about.cljs
+++ src/cljs/tmhas/panels/about.cljs
@@ -1,27 +1,28 @@
 (ns tmhas.panels.about
   (:require [re-frame.core :as rf]
-            [re-com.core :as re-com]
-            [goog.string :as gstring]
-            goog.string.format))
-
-(def entry-id "eJCWlzmH8Ai2euGY2eQm4")
+            [re-com.core :as re-com]))
 
 (def query
-  (gstring/format "{ aboutSections(q: \"sys.id=%s\") {
+  "{ aboutSections {
 			 sectionTitle, sectionText
 		 }
-	}" entry-id))
+	}")
 
 (defn about-panel []
       (let [ref-key :about-panel]                                ;; 0. declare unique ref-key
            (rf/reg-sub ref-key #(ref-key %))                     ;; 1. register subscriber ref-key
            (rf/dispatch [:get-contentful-data ref-key query])    ;; 2. retrieve contentful data & pass key for assoc in db
 
-           (let [aboutSections (first (:aboutSections @(rf/subscribe [ref-key])))] ;; 3. react to retrieved data
+           (let [aboutSections (:aboutSections @(rf/subscribe [ref-key]))] ;; 3. react to retrieved data
                 [re-com/v-box
                  :children
-                 [[:h1
-                   {:class "bb bw1"}
-                   (:sectionTitle aboutSections)]
-                  ;; TODO: needs markdown parser here
-                  [:p (:sectionText aboutSections)]]])))
+                 ;; TODO: needs markdown parser here
+                 [(for [section aboutSections
+                         :let [title (:sectionTitle section)
+                               text  (:sectionText section)]]
+                    ^{:key (gensym "aboutSection-")}
+                     [:div
+                      [:h1
+                       {:class "bb bw1"}
+                       title]
+                      [:p text]])]])))
