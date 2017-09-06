90cba96358bf86eea31fe912c7543afb98377473
David A. Viramontes <dviramontes@gmail.com>
2017-09-06 17:07:21 -0600

    refactor: :get-contentful-data event to switch between local and stage (#30)
    dev uris depending on enviornment

--- src/cljs/tmhas/events.cljs
+++ src/cljs/tmhas/events.cljs
@@ -3,28 +3,30 @@
   (:require [re-frame.core :as rf]
             [ajax.core :as ajax :refer [GET]]
             [day8.re-frame.http-fx]
-            [tmhas.db :as db]))
+            [tmhas.db :as db]
+            [tmhas.config :as config]))
 
 (env/def API_KEY "EVENTUALLY")
 
 (rf/reg-event-fx
   :get-contentful-data
-  (fn [{db :db} [_ ref-key query]]
+  (fn [{db :db} [_ rf-key query]]
       ;; TODO: add loading state...
-      {:db         db
-       :http-xhrio {:method          :get
-                    ;:headers         {:Authorization (str "Bearer " API_KEY)}
-                    :format          (ajax/json-request-format)
-                    :params          {:query query}
-                    :uri             "http://localhost:4000/graphql" ;; local dev server
-                    ; :uri             "https://node-project-starter-hxumagngjh.now.sh/graphql" ;; stage dev server
-                    :response-format (ajax/json-response-format {:keywords? true})
-                    :on-success      [:get-contentful-data-success ref-key]}}))
+      (let [uri (if config/debug? "http://localhost:4000/graphql"
+                                  "https://node-project-starter-hxumagngjh.now.sh/graphql")]
+        {:db         db
+         :http-xhrio {:method          :get
+                      ;:headers         {:Authorization (str "Bearer " API_KEY)}
+                      :format          (ajax/json-request-format)
+                      :params          {:query query}
+                      :uri             uri
+                      :response-format (ajax/json-response-format {:keywords? true})
+                      :on-success      [:get-contentful-data-success rf-key]}})))
 
 (rf/reg-event-db
   :get-contentful-data-success
-  (fn [db [_ ref-key & [{data :data}]]]
-      (assoc db ref-key data)))
+  (fn [db [_ rf-key & [{data :data}]]]
+      (assoc db rf-key data)))
 
 
 (rf/reg-event-db
