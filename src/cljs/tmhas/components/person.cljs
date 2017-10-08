85f31227b42118606491f545c63a22d0d0949c00
David Viramontes <dviramontes@gmail.com>
2017-10-07 22:50:25 -0600

    feat: populate person component with graphql data

--- src/cljs/tmhas/components/person.cljs
+++ src/cljs/tmhas/components/person.cljs
@@ -1,13 +1,13 @@
 (ns tmhas.components.person
   (:require [re-com.core :as re-com]))
 
-(defn person []
+(defn person [data]
       [:div {:class "flexrow-wrap w-100 mv3"}
          [:div {:class "w-100 w-25-ns tc"}
-             [:img {:src "/img/candi.png"
+             [:img {:src (-> data :image :url)
                     :class "w-70 w-100-ns br-100 mb3"}]]
          [re-com/v-box
           :class "w-100 w-75-ns pl4-ns"
-          :children [[:h2 {:class "mt0 f2 f3-ns tc"}
-                         "Candi CdeBaca"]
-                     [:p "Candi CdeBaca is Co-Founder and Co-Executive Director of Project VOYCE, Founder & Member of Cross Community Coalition and Founder & Principal of Rebel Soul Strategies. She is a Community Advocate in her home community of Northeast Denver. She has been featured as an influential leader and advocate by several publications and outlets including Politico, Fortune, Forbes, New York Times, The Guardian, The Atlantic, 5280 Magazine, and Canadian Broadcast Corporation. She has also authored and co-authored publications featured in the Nonprofit Quarterly and Equity Alliance. She is a fierce advocate for equity. CdeBaca has an unwavering commitment to ensuring increased participation of underrepresented groups in the political process and in leadership and decision-making roles across sectors."]]]])
+          :children [[:h2 {:class "mt0 f2 f3-ns"}
+                         (:name data)]
+                     [:p (:bio data)]]]])
