7d77d13084d43958c019008aee6e23860227be53
eemshi <oreneemshi@gmail.com>
2017-10-18 19:11:30 -0600

    begins scaffolding media thumbnail

--- src/cljs/tmhas/panels/home.cljs
+++ src/cljs/tmhas/panels/home.cljs
@@ -1,8 +1,13 @@
 (ns tmhas.panels.home
   (:require [re-frame.core :as re-frame]
-            [re-com.core :as re-com]))
+            [re-com.core :as re-com]
+            [tmhas.components.media-thumb :refer [media-thumb]]
+            [tmhas.components.common :refer [showdown]]))
 
 (defn home-panel []
       [re-com/v-box
        :children
-       [[:h1 {:class "bb bw1"} "Latest"]]])
+       [[:h1 {:class "bb bw1"} "Latest"]
+        [:div {:class "flexrow-wrap w-100 mv3"}
+              [media-thumb]
+              [media-thumb]]]])
