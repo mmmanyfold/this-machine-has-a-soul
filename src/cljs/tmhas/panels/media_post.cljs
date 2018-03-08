4950d8db1b1508ce343cb09e3eb529590052de80
Michelle L <oreneemshi@gmail.com>
2018-03-07 22:09:10 -0700

    adds gradient transparency to nav bar, fixes tags & extra page width (#45)
    adds gradient to nav bar, fixes tags, rm extra page width

--- src/cljs/tmhas/panels/media_post.cljs
+++ src/cljs/tmhas/panels/media_post.cljs
@@ -79,8 +79,13 @@
                              [:h2 {:class "mt3 mb1 mh1 f3 f2-ns"}
                                   postTitle]
                              [:div {:class "metadata f5"}
-                               [:span postDate][:span "•"]
-                               [:span "#Cole"] [:span "#Art"] [:span "#Process"]
+                               [:span postDate]
+                               (when tags
+                                 [:section.tags
+                                  [:span "•"]
+                                  (for [tag tags]
+                                       ^{:key (gensym "tag-")}
+                                       [:span (str "#" tag)])])
                                [:div {:class "f4 mh1 mt2"
                                       "dangerouslySetInnerHTML"
                                       #js{:__html (.makeHtml showdown postText)}}]]
