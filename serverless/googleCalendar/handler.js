2665c9a4287fc1c6159f872c5c7e6250da9fce1e
David A. Viramontes <dviramontes@gmail.com>
2018-04-22 00:45:40 -0600

    feat: serverless fn for google calendar events (#52)
    * feat: retrieve gcal events from api
    
    * update: maxResults
    
    * rm: console.log

---
+++ serverless/googleCalendar/handler.js
@@ -0,0 +1,50 @@
+import { google } from 'googleapis';
+
+// overwrite env var path
+process.env['GOOGLE_APPLICATION_CREDENTIALS'] = process.cwd() + '/creds.json';
+
+const calendar = google.calendar('v3');
+const scopes = ['https://www.googleapis.com/auth/calendar.readonly'];
+const calendarId = process.env.CALENDAR_ID;
+
+
+const fetchEventList = async () => {
+    // This method looks for the GCLOUD_PROJECT and GOOGLE_APPLICATION_CREDENTIAL environment variables
+    const auth = await google.auth.getClient({
+        scopes,
+    });
+
+    // obtain the current project Id
+    const project = await google.auth.getDefaultProjectId();
+
+    // Fetch the list of GCE zones within a project.
+    try {
+        return await calendar.events.list({
+            project,
+            auth,
+            calendarId,
+            timeMin: (new Date()).toISOString(),
+            showDeleted: false,
+            singleEvents: true,
+            // maxResults: 25,
+            orderBy: 'startTime',
+        });
+    } catch (e) {
+        throw new Error(e);
+    }
+};
+
+const main = async () => {
+    try {
+        const { data: { items } } = await fetchEventList();
+        return items;
+    } catch (e) {
+        throw new Error(e);
+    }
+}
+
+export const events = async (event, context, callback) =>
+    callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(await main()),
+    });
