73ab4b24f91ba4fb8f18374bdf6209a94b76d234
eemshi <oreneemshi@gmail.com>
2018-11-01 00:23:48 -0600

    add social media links

--- src/cljs/tmhas/components/footer.cljs
+++ src/cljs/tmhas/components/footer.cljs
@@ -3,8 +3,16 @@
             [re-frame.core :as rf]))
 
 (defn footer []
-  [:div {:class "footer bt mh3 mh4-ns ph1 pv3 tr"}
-   [:span {:on-click #(js/window.showMailingPopUp)
-           :class "f4 b ttu pointer pb1"
-           :style {:letter-spacing "1px"}}
-      "Join Mailing List >>"]])
+  [:div {:class "footer bt mh3 mh4-ns ph1 pv3 flex justify-between"}
+   [:div
+    [:a.social-link {:href "https://www.facebook.com/warmcookiesoftherevolution"}
+     [:img {:src "icons/fb.svg"}]]
+    [:a.social-link {:href "https://www.instagram.com/warmcookiesrevolution/"}
+     [:img {:src "icons/ig.svg"}]]
+    [:a.social-link {:href "https://vimeo.com/user24711262"}
+     [:img {:src "icons/vimeo.svg"}]]]
+   [:div
+    [:span {:on-click #(js/window.showMailingPopUp)
+            :class "f4 b ttu pointer pb1"
+            :style {:letter-spacing "1px"}}
+       "Join Mailing List >>"]]])
